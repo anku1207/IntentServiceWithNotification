296079b5db578b281d2a1dfcc2cab5dbb8a01908
Your Name <you@example.com>
2021-12-13 18:17:20 +0530

    add

--- testmodule/src/main/java/com/example/testmodule/Module_MainActivity.kt
+++ testmodule/src/main/java/com/example/testmodule/Module_MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.example.testmodule.databinding.ModuleActivityMainBinding
 
 class Module_MainActivity : AppCompatActivity() {
     lateinit var module_btn:Button
+    lateinit var mainBinding: ModuleActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.module_activity_main)
 
-        module_btn=findViewById(R.id.module_btn);
 
-        module_btn.setOnClickListener(object : View.OnClickListener{
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.module_activity_main)
+
+      //  module_btn=findViewById(R.id.module_btn);
+
+        mainBinding.moduleBtn.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
                 val intent = Intent()
                 intent.putExtra("value","sdfsljdf")
